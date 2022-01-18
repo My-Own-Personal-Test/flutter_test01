0ea284b0ce7c17684b437d9187f45d95bc3baf63
ryansyrl <m.ridwan.riansyah@gmail.com>
2022-01-18 15:57:11 +0700

    customize widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import './questions.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (questionIndex != 1) {
+        questionIndex = questionIndex + 1;
+      } else {
+        questionIndex = questionIndex - 1;
+      }
+      print(questionIndex);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    var questions = [
+      'is dart fun?',
+      'is dart dificult?',
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Project'),
+      ),
+      body: Column(
+        children: [
+          Question(questions[questionIndex]),
+          RaisedButton(
+            child: Text('Answer 1!'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2!'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3!'),
+            onPressed: () {
+              print('funct alt 3');
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
